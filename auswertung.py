d2037db4db0988ba0ce8be7186d1b292ac4259eb
Kevin Schmidt <kevin3.schmidt@tu-dortmund.de>
2016-11-03 09:31:27 +0100

    auswertung erweitert.

--- auswertung.py
+++ auswertung.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from uncertainties import ufloat
 import uncertainties.unumpy as unp
 from scipy.optimize import curve_fit
-
+import scipy.odr.odrpack as odrpack
 # Konstanten
 
 l = 1.54286
@@ -22,7 +22,7 @@ Ncaesiumchlorid = np.array([2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26])
 # Einlesen der Daten #
 ######################
 
-r , rerr = np.genfromtxt('werte1.txt', unpack=True)
+r , rerr = np.genfromtxt('werte1m.txt', unpack=True)
 
 rges = unp.uarray(r, rerr)
 
@@ -48,92 +48,165 @@ a = d * (Ndiamant)**(0.5)
 print('a in Angström')
 print(a)
 
-theta = unp.nominal_values(theta)
+theta1 = unp.nominal_values(theta)
+theta2 = unp.std_devs(theta)
 a1 = unp.nominal_values(a)
+a2 = unp.std_devs(a)
+
+#print(theta1, theta2)
 
 
+def f(theta1, m, b):
+    return m * (np.cos((2*np.pi)/360*theta1))**2  + b
 
-def f(theta,m , b):
-    return m * (np.cos((2*np.pi)/360*theta))**2  + b
-
-params, covariance = curve_fit(f, theta, a1)
-
+params, covariance = curve_fit(f, theta1, a1)
 
 errors = np.sqrt(np.diag(covariance))
 
 print('m =', params[0], '\pm', errors[0], 'in Angström')
 print('b =', params[1], '\pm', errors[1], 'in Angström')
 
-x = np.linspace(0, 1)
+################################################################################
+##
+##    Test orth. regression
+##
+################################################################################
+
+def f(B, theta1):
+    return B[0] * (np.cos((2*np.pi)/360*theta1))**2  + B[1]
+
+x = theta1
+y = a1
+sx = theta2
+sy = a2
+
+linear = odrpack.Model(f)
+
+myData = odrpack.RealData(x, y, sx=sx, sy=sy)
+myOdr = odrpack.ODR(myData, linear , beta0=[1,1])
+myOdr.set_job(fit_type=2) #if set fit_type=2, returns the same as leastsq
+out = myOdr.run()
+#out.pprint()
+
+coeff = out.beta[::-1]
+err   = out.sd_beta[::-1]
+
+print(coeff[0], err[0])
+print(coeff[1], err[1])
+
+################################################################################
+
+
+
+#print(np.cos(np.deg2rad(theta2))**2)
+
+x = np.linspace(0, 1, 10000)
 
 plt.figure(figsize=(10,8))
 
-plt.errorbar(np.cos((2*np.pi)/360*theta)*np.cos((2*np.pi)/360*theta), unp.nominal_values(a), yerr=unp.std_devs(a), fmt='bx', label='Messwerte')
-#plt.plot(np.cos((2*np.pi)/360*theta)*np.cos((2*np.pi)/360*theta), a, 'bx', label='Messwerte')
-plt.plot(x, params[0]*x+params[1], 'r-', label='Ausgleichsgerade')
+plt.errorbar(np.cos((2*np.pi)/360*theta1)*np.cos((2*np.pi)/360*theta1), unp.nominal_values(a),xerr=2*np.cos(np.deg2rad(theta1))*np.sin(np.deg2rad(theta1))*np.deg2rad(theta2), yerr=unp.std_devs(a), fmt='bx', label='Messwerte')
+#plt.plot(np.cos((2*np.pi)/360*theta1)**2, coeff[1]*x+coeff[0], 'bx', label='Messwerte')
+plt.plot(x, coeff[1]*x+coeff[0], 'r-', label='Ausgleichsgerade')
 plt.legend(loc=2)
 plt.savefig('probe1.pdf')
 
 
 
-#########################
-#                       #
-#        Probe 2        #
-#                       #
-#########################
-
-######################
-# Einlesen der Daten #
-######################
-
-r, rerr = np.genfromtxt('werte2.txt', unpack=True)
-
-rges = unp.uarray(r, rerr)
-
-theta = rges/2
-
-print('theta')
-print(theta)
-
-d = l/(2 * unp.sin(2*np.pi*theta/360))
-
-d1 = l/(2 * unp.sin(2*np.pi*theta[0]/360))
-
-print('d in Angström')
-print(d)
-
-verhaeltnis = d1 / d
-
-print('d1/d')
-print(verhaeltnis)
-
-a = d * (Ncaesiumchlorid)**(0.5)
-
-print('a in Angström')
-print(a)
-
-theta = unp.nominal_values(theta)
-a1 = unp.nominal_values(a)
-
-
-
-def f(theta,m , b):
-    return m * (np.cos((2*np.pi)/360*theta))**2  + b
-
-params, covariance = curve_fit(f, theta, a1)
-
-
-errors = np.sqrt(np.diag(covariance))
-
-print('m =', params[0], '\pm', errors[0], 'in Angström')
-print('b =', params[1], '\pm', errors[1], 'in Angström')
-
-x = np.linspace(0, 1)
-
-plt.figure(figsize=(10,8))
-
-plt.errorbar(np.cos((2*np.pi)/360*theta)*np.cos((2*np.pi)/360*theta), unp.nominal_values(a), yerr=unp.std_devs(a), fmt='bx', label='Messwerte')
-#plt.plot(np.cos((2*np.pi)/360*theta)*np.cos((2*np.pi)/360*theta), a, 'bx', label='Messwerte')
-plt.plot(x, params[0]*x+params[1], 'r-', label='Ausgleichsgerade')
-plt.legend(loc=2)
-plt.savefig('probe2.pdf')
+# #########################
+# #                       #
+# #        Probe 2        #
+# #                       #
+# #########################
+#
+# ######################
+# # Einlesen der Daten #
+# ######################
+#
+# r, rerr = np.genfromtxt('werte2m.txt', unpack=True)
+#
+# rges = unp.uarray(r, rerr)
+#
+# theta = rges/2
+#
+# print('theta')
+# print(theta)
+#
+# d = l/(2 * unp.sin(2*np.pi*theta/360))
+#
+# d1 = l/(2 * unp.sin(2*np.pi*theta[0]/360))
+#
+# print('d in Angström')
+# print(d)
+#
+# verhaeltnis = d1 / d
+#
+# print('d1/d')
+# print(verhaeltnis)
+#
+# a = d * (Ncaesiumchlorid)**(0.5)
+#
+# print('a in Angström')
+# print(a)
+#
+# theta1 = unp.nominal_values(theta)
+# theta2 = unp.std_devs(theta)
+# a1 = unp.nominal_values(a)
+# a2 = unp.std_devs(a)
+#
+# #print(theta1, theta2)
+#
+#
+# def f(theta1, m, b):
+#     return m * (np.cos((2*np.pi)/360*theta1))**2  + b
+#
+#
+# params, covariance = curve_fit(f, theta1, a1)
+#
+#
+# errors = np.sqrt(np.diag(covariance))
+#
+# print('m =', params[0], '\pm', errors[0], 'in Angström')
+# print('b =', params[1], '\pm', errors[1], 'in Angström')
+#
+#
+# ################################################################################
+# ##
+# ##    Test orth. regression
+# ##
+# ################################################################################
+#
+# def f(B, theta1):
+#     return B[0] * (np.cos((2*np.pi)/360*theta1))**2  + B[1]
+#
+# x = theta1
+# y = a1
+# sx = theta2
+# sy = a2
+#
+# linear = odrpack.Model(f)
+#
+# myData = odrpack.RealData(x, y, sx=sx, sy=sy)
+# myOdr = odrpack.ODR(myData, linear , beta0=[1,1])
+# myOdr.set_job(fit_type=2) #if set fit_type=2, returns the same as leastsq
+# out = myOdr.run()
+# #out.pprint()
+#
+# coeff = out.beta[::-1]
+# err   = out.sd_beta[::-1]
+#
+# print(coeff[0], err[0])
+# print(coeff[1], err[1])
+#
+# ################################################################################
+#
+#
+#
+# x = np.linspace(0, 1)
+#
+# plt.figure(figsize=(10,8))
+#
+# plt.errorbar(np.cos((2*np.pi)/360*theta1)**2, unp.nominal_values(a), xerr=np.cos((2*np.pi)/360*theta2)**2, yerr=unp.std_devs(a), fmt='bx', label='Messwerte')
+# #plt.plot(np.cos((2*np.pi)/360*theta)*np.cos((2*np.pi)/360*theta), a, 'bx', label='Messwerte')
+# plt.plot(x, coeff[1]*x+coeff[0], 'r-', label='Ausgleichsgerade')
+# plt.legend(loc=2)
+# plt.savefig('probe2.pdf')
